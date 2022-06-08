9ab345018ee1f62acee022e8da44a330f24f991a
Daniel Calle <danielcalleco@gmail.com>
2022-06-07 20:06:17 -0500

    Se crea index.js y comienza la logica

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const text = document.getElementById("textarea");
+const encriptarbtn = document.getElementById("encriptarBtn");
+const desencriptarbtn = document.getElementById("desencriptarBtn");
+
+const resultArea = document.getElementById("result");
+
+encriptarbtn.addEventListener("click", () => {
+    resultArea.innerHTML = `
+    <div class="main__resultZone--withText">
+    <div class="body__main--finalText">
+    <!--todo Aquí irá el texto final -->
+    </div>
+    <button class="main__resultZone--copybtn">Copiar</button>
+    </div>`;
+});
+
+function encriptar() {
+    const textToEncode = text.value;
+}
